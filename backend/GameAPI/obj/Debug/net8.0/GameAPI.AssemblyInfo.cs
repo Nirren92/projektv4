ead00d7ffa3940976f4e7775b9ec9282e76a934d
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2024-11-06 19:52:47 +0100

    lagt till flow

--- backend/GameAPI/obj/Debug/net8.0/GameAPI.AssemblyInfo.cs
+++ backend/GameAPI/obj/Debug/net8.0/GameAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f71c678497eb5516afdb35c407088f1f5158152")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8034da91f206aa802472ed9841ad8caf20de3644")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
